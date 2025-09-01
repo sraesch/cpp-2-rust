da17e779fa6597d5f1659b97768604e21a0a0d01
Sascha Raesch <sascha.raesch@gmail.com>
2025-09-01 08:31:23 +0200

    ✨ Added logging

--- cpp2rust-app/src-tauri/src/lib.rs
+++ cpp2rust-app/src-tauri/src/lib.rs
@@ -1,22 +1,25 @@
 use std::{collections::BTreeMap, path::Path};
 
 use cpp2rust::cpp::{CMakeCache, CMakeVariable};
+use log::{debug, info, log_enabled, Level};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn load_cache(folder: &str) -> Option<CMakeCache> {
+    info!("Loading CMake cache from folder: {}", folder);
+
     let p = Path::new(folder);
 
     // check if the folder exists and is a directory
     if !p.exists() || !p.is_dir() {
-        println!("Folder does not exist or is not a directory: {}", folder);
+        info!("Folder does not exist or is not a directory: {}", folder);
         return None;
     }
 
     // check if the CMakeCache.txt file exists
     let cache_file = p.join("CMakeCache.txt");
     if !cache_file.exists() {
-        println!(
+        info!(
             "CMakeCache.txt file does not exist: {}",
             cache_file.display()
         );
@@ -34,17 +37,28 @@ type Entries = BTreeMap<String, CMakeVariable>;
 #[tauri::command]
 fn configure_cmake(source_dir: &str, build_dir: &str, entries: Entries) {
     // Implement the CMake configuration logic here
-    println!("Configuring CMake with:");
-    println!("Source Directory: {}", source_dir);
-    println!("Build Directory: {}", build_dir);
-    for (name, value) in entries.iter() {
-        println!("Entry: {} = {:?}", name, value);
+    info!("Configuring CMake with:");
+    info!("Source Directory: {}", source_dir);
+    info!("Build Directory: {}", build_dir);
+    if log_enabled!(Level::Debug) {
+        for (name, value) in entries.iter() {
+            debug!("Entry: {} = {:?}", name, value);
+        }
     }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Determine the log level using the environment variable
+    let log_level = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into());
+    let log_level = log_level.parse().unwrap_or(Level::Info);
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .filter(move |e| e.level() <= log_level)
+                .build(),
+        )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![load_cache, configure_cmake])
